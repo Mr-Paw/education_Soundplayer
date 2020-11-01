b69360d6ba529f0daf23c4460fb651e5b6eb23b3
Paul <vasil23876@yandex.ru>
2020-11-01 14:03:48 +0300

    commit

---
+++ soundplayer/SoundPlayer.swift
@@ -0,0 +1,32 @@
+//
+//  SoundPlayer.swift
+//  soundplayer
+//
+//  Created by paw on 01.11.2020.
+//  Copyright © 2020 Paul Moryakov. All rights reserved.
+//
+
+import AVFoundation
+struct Soundplayer {
+    private var soundToPlay: URL?
+    private var audioPlayer = AVAudioPlayer()
+    init?(forFileName fileName: String, withExtension: String){
+        guard soundToPlay != nil else {return nil}
+        soundToPlay = Bundle.main.url(forResource: fileName, withExtension: withExtension)
+        do {
+             audioPlayer = try AVAudioPlayer(contentsOf: soundToPlay!)
+         audioPlayer.prepareToPlay()
+         } catch {
+             print("couldn't load sound file")
+         }
+    }
+    var isPlaying: Bool {return audioPlayer.isPlaying}
+    
+    func play(){audioPlayer.play()}
+    func pause(){audioPlayer.pause()}
+    func stop(){audioPlayer.stop()}
+    func playIfStoppedOrPaused(){if !isPlaying {play()}}
+    func stopIfPlaying(){if isPlaying {stop()}}
+    func pauseIfPlaying(){if isPlaying {pause()}}
+    func playIfStoppedOrPausedOrStopIfPlaying(){if !isPlaying {play()}else{stop()}}
+}
